12575e3e69728a7c5b87daa206411ecf55562d55
jameel <shaikjameel5373@gmail.com>
2023-12-18 23:40:39 +0530

    corrected few commands

--- DISK_USAGE.sh
+++ DISK_USAGE.sh
@@ -7,8 +7,8 @@ message=""
 while IFS= read line
 do
 usage=$(echo $line |  awk '{print$6F}' | cut -d % -f1)
-partition=$(echo $line |  awk '{print$1F}' | cut -d % -f1)
-if [$usage -gt $DISK_THRESHOLD]
+partition=$(echo $line |  awk '{print$1F}')
+if [ $usage -ge $DISK_THRESHOLD ]
 then
 message+="High disk usage on $partition: $usage\n"
 fi
