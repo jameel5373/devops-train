a9a9d032c84623a66a0b8b9d97aa6ddab14b4fb6
jameel <shaikjameel5373@gmail.com>
2023-12-20 01:26:07 +0530

    edietd ip line as ip not reflecting in op

--- Roboshop.sh
+++ Roboshop.sh
@@ -14,7 +14,7 @@ do
     INSTANCE_TYPE="t2.micro"
     fi
 
-   IP_ADDRESS=$(aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids sg-04e5362f0310fc7bf --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instance[0].PrivateIpAddress' --output text)
+   IP_ADDRESS=$(aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids sg-04e5362f0310fc7bf --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
 
    echo "$i: $IP_ADDRESS"
 
